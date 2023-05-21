2dd05a356bf86554f09e238a37de75f0d7a9f04f
VidulaVVD <viduladeshpandevvd@gmail.com>
2023-05-21 11:39:24 +0530

    FINAL CODE

---
+++ config/db.js
@@ -0,0 +1,12 @@
+ import mongoose from "mongoose";
+ import colors from "colors";
+
+ const connectDB = async () => {
+   try {
+        const conn = await mongoose.connect(process.env.Mongo_URL);
+        console.log(`connected to mongodb database ${conn.connection.host}`);
+   } catch (error) {
+        console.log(`Error in mongodb ${error}` .bgRed.white);
+   }
+ };
+ export default connectDB;
